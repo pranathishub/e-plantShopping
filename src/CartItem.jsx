5029f6968200dad758244ab0192aa11ef5b4f7f2
pranathishub <2420030050@klh.edu.in>
2026-03-15 19:31:44 +0530

    Fixed assignment requirements

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,14 +1,55 @@
-function CartItem({item}) {
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increaseQty, decreaseQty, removeItem } from "./CartSlice";
+
+function CartItem() {
+
+  const cart = useSelector((state) => state.cart.items);
+
+  const dispatch = useDispatch();
+
+  const totalAmount = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
   return (
     <div>
-      <h3>{item.name}</h3>
-      <p>Price: ${item.price}</p>
 
-      <button>-</button>
-      <span>1</span>
-      <button>+</button>
+      <h2>Shopping Cart</h2>
 
-      <button>Delete</button>
+      {cart.map((item) => (
+
+        <div key={item.id}>
+
+          <img src={item.image} alt={item.name} width="80"/>
+
+          <h3>{item.name}</h3>
+
+          <p>Price: ${item.price}</p>
+
+          <p>Total: ${item.price * item.quantity}</p>
+
+          <button onClick={() => dispatch(decreaseQty(item.id))}>-</button>
+
+          {item.quantity}
+
+          <button onClick={() => dispatch(increaseQty(item.id))}>+</button>
+
+          <button onClick={() => dispatch(removeItem(item.id))}>
+            Delete
+          </button>
+
+        </div>
+
+      ))}
+
+      <h3>Total Cart Amount: ${totalAmount}</h3>
+
+      <button>Checkout (Coming Soon)</button>
+
+      <button>Continue Shopping</button>
+
     </div>
   );
 }
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,23 +1,60 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "./CartSlice";
+
+const plants = {
+  Indoor: [
+    { id: 1, name: "Snake Plant", price: 10, image: "plant1.jpg" },
+    { id: 2, name: "Peace Lily", price: 12, image: "plant2.jpg" },
+    { id: 3, name: "Spider Plant", price: 8, image: "plant3.jpg" },
+    { id: 4, name: "Rubber Plant", price: 15, image: "plant4.jpg" },
+    { id: 5, name: "ZZ Plant", price: 18, image: "plant5.jpg" },
+    { id: 6, name: "Pothos", price: 9, image: "plant6.jpg" }
+  ],
+
+  Outdoor: [
+    { id: 7, name: "Rose", price: 20, image: "plant7.jpg" },
+    { id: 8, name: "Lavender", price: 14, image: "plant8.jpg" },
+    { id: 9, name: "Tulip", price: 11, image: "plant9.jpg" },
+    { id: 10, name: "Sunflower", price: 13, image: "plant10.jpg" },
+    { id: 11, name: "Jasmine", price: 16, image: "plant11.jpg" },
+    { id: 12, name: "Hibiscus", price: 17, image: "plant12.jpg" }
+  ]
+};
+
 function ProductList() {
 
-  const plants = [
-    {id:1, name:"Aloe Vera", price:10, category:"Succulents"},
-    {id:2, name:"Snake Plant", price:15, category:"Indoor"},
-    {id:3, name:"Peace Lily", price:18, category:"Indoor"},
-    {id:4, name:"Rose", price:12, category:"Outdoor"},
-    {id:5, name:"Cactus", price:8, category:"Succulents"},
-    {id:6, name:"Tulip", price:14, category:"Outdoor"}
-  ];
+  const dispatch = useDispatch();
+
+  const handleAdd = (plant) => {
+    dispatch(addItem(plant));
+  };
 
   return (
     <div>
-      <h2>Plant List</h2>
 
-      {plants.map((plant) => (
-        <div key={plant.id}>
-          <h3>{plant.name}</h3>
-          <p>${plant.price}</p>
-          <button>Add to Cart</button>
+      {Object.keys(plants).map((category) => (
+        <div key={category}>
+          <h2>{category}</h2>
+
+          <div>
+            {plants[category].map((plant) => (
+              <div key={plant.id}>
+
+                <img src={plant.image} alt={plant.name} width="100"/>
+
+                <h3>{plant.name}</h3>
+
+                <p>${plant.price}</p>
+
+                <button onClick={() => handleAdd(plant)}>
+                  Add to Cart
+                </button>
+
+              </div>
+            ))}
+          </div>
+
         </div>
       ))}
 
