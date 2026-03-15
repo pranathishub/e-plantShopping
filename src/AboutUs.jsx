1c70b0ef1ab1fe4e10e2e88598a2a23824dcafaf
pranathi <2420030050@klh.edu.in>
2026-03-15 20:44:08 +0530

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -1,10 +1,26 @@
+import React from "react";
+
 function AboutUs() {
   return (
-    <div>
-      <h1>Paradise Nursery</h1>
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+
       <p>
-        Paradise Nursery offers a variety of beautiful houseplants that bring
-        life and freshness to your home.
+        Paradise Nursery is dedicated to bringing nature closer to people by
+        offering a wide range of beautiful indoor houseplants. Our goal is to
+        help customers create greener, healthier living spaces.
+      </p>
+
+      <p>
+        We believe plants not only enhance the beauty of homes and offices but
+        also improve well-being and air quality. Our collection includes plants
+        suitable for beginners as well as experienced plant lovers.
+      </p>
+
+      <p>
+        At Paradise Nursery, we strive to provide high-quality plants along
+        with a seamless online shopping experience through our e-plantShopping
+        platform.
       </p>
     </div>
   );
